a31214667dbb3c3ecb20acabc67afab5de2262e3
grant-pie <grant.pieterse1@gmail.com>
2025-11-24 11:25:03 +0200

    Added wiki page

---
+++ Docs_plus_pre_nuxt/Docs/OhmRGB - Livid Wiki_files/load(1).php
@@ -0,0 +1,22 @@
+@import url(https://web.archive.org/web/20150501223552cs_/http://fonts.googleapis.com/css?family=Raleway:400,700);*{font-family:"Raleway","HelveticaNeue-Light","Helvetica Neue Light","Helvetica Neue",Helvetica,Arial,"Lucida Grande",sans-serif}body{font-size:120%}
+/* cache key: wiki_lividinstruments_co-35891_:resourceloader:filter:minify-css:7:d3f0b4be303b09170f433a03bdd2ed4d */
+/*
+     FILE ARCHIVED ON 22:35:52 May 01, 2015 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 08:18:45 Nov 24, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 1.042
+  exclusion.robots: 0.049
+  exclusion.robots.policy: 0.026
+  esindex: 0.014
+  cdx.remote: 10.512
+  LoadShardBlock: 208.249 (3)
+  PetaboxLoader3.datanode: 145.915 (4)
+  PetaboxLoader3.resolve: 130.141 (3)
+  load_resource: 82.659
+*/
---
+++ Docs_plus_pre_nuxt/Docs/OhmRGB - Livid Wiki_files/load(4).php
@@ -0,0 +1,31 @@
+var _____WB$wombat$assign$function_____=function(name){return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name))||self[name];};if(!self.__WB_pmw){self.__WB_pmw=function(obj){this.__WB_source=obj;return this;}}{
+let window = _____WB$wombat$assign$function_____("window");
+let self = _____WB$wombat$assign$function_____("self");
+let document = _____WB$wombat$assign$function_____("document");
+let location = _____WB$wombat$assign$function_____("location");
+let top = _____WB$wombat$assign$function_____("top");
+let parent = _____WB$wombat$assign$function_____("parent");
+let frames = _____WB$wombat$assign$function_____("frames");
+let opens = _____WB$wombat$assign$function_____("opens");
+mw.loader.state({"site":"ready"});
+/* cache key: wiki_lividinstruments_co-35891_:resourceloader:filter:minify-js:7:507d50f037763c9b2048d374c6b390f9 */
+}
+/*
+     FILE ARCHIVED ON 22:35:45 May 01, 2015 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 08:18:47 Nov 24, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 2.999
+  exclusion.robots: 0.073
+  exclusion.robots.policy: 0.02
+  esindex: 0.009
+  cdx.remote: 35.191
+  LoadShardBlock: 191.782 (3)
+  PetaboxLoader3.datanode: 149.728 (4)
+  load_resource: 14.687
+*/
